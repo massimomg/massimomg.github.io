09356156719739aecd6b9994004fb7d9e04ff3fc
massimomg <73030456+massimomg@users.noreply.github.com>
2020-12-11 22:42:18 -0500

    Delete Datos.js

--- JS/Datos.js
+++
@@ -1,5 +0,0 @@
-var btnBack = document.getElementById('btnBack');
-btnBack.onclick = function cmo(){
-    document.body.classList.toggle('BgClass');
-    document.getElementById("tablaObjetos").classList.toggle("Menu_y_nombre_oscuro")
-}
